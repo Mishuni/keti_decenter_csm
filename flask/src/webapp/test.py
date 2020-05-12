b3d455bb90c168fa106e101d931fc57cf20b62fa
Mishuni <miseon543@gmail.com>
2020-05-12 17:41:40 +0900

    Update

--- flask/src/webapp/test.py
+++ flask/src/webapp/test.py
@@ -5,7 +5,7 @@ import requests, json
 # http://ptsv2.com/t/ucjg5-158907602/post
 # "http://127.0.0.1:9000"+"/result"
 # "http://182.252.132.39:9000"+"/result"
-url = "http://127.0.0.1:5005"+"/result"
+url = "http://182.252.132.39:9000"+"/result"
 # url = 'http://ptsv2.com/t/ucjg5-158907602/post'
 data = {'confidence':0.9,'groupName':'B'}
 headers = {'Content-Type': 'application/json; charset=utf-8'}
@@ -15,7 +15,7 @@ print(r.text)
 
 ## 그냥 몸체 통으로 보내기
 #url = "http://182.252.132.39:9000"+"/result"
-data = {'groupName':'A','confidence':0.9}
+data = {'groupName':'B','confidence':0.9}
 #headers = {'Content-Type': 'application/json; charset=utf-8'}
-r = requests.post(url, data = data) #headers=headers)    
+r = requests.post(url, data = data)#,headers=headers)    
 print(r.text)
